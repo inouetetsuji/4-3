edaf21d54110e87eaaa9a08cf1e5ff47d0c884a8
inouetetsuji <81937796+inouetetsuji@users.noreply.github.com>
2023-06-24 16:00:22 +0900

    Add files via upload

---
+++ sqlTask6.sql
@@ -0,0 +1,7 @@
+
+-- 6. 商品テーブルに「商品コード='M001'、商品名='マフラー'、単価=4500円、更新日付=本日日付」のデータを追加しなさい。※実行後のSELECT結果も貼付すること。
+-- [回答]
+INSERT INTO goods_table
+(goods_code,goods_name,price,update_day)
+VALUES
+('M001','マフラー',4500,'2023/06/24');
